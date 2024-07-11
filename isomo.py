06f3f610aa508267041b17868ca2cddf63f43ea8
TWAGIRUMUKIZA FIDELE <twagirafidele30@gmail.com>
2024-07-11 19:09:11 +0000

    to study

---
+++ isomo.py
@@ -0,0 +1,14 @@
+name="FIDELE"
+print(name)
+
+registrationNumber=215017221
+print(registrationNumber)
+
+dateofbirth="21/04/1995"
+
+# adress=lebanon
+# city=ashairman
+# home phone number=0539919319
+# score in subject 1=84
+# score in subject 2= 90
+# average score= 87
